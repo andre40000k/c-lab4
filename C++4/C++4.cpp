011586c6400d61553238ae85238c01fbaf551dc7
Андрей Будяну <andre40000k@gmail.com>
2019-05-06 17:45:52 +0300

    Добавьте файлы проекта.

---
+++ C++4/C++4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include "Windows.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	string a;
+	cin >> a;
+	string b;
+	cin >> b;
+	string c = a + b;
+	cout << c;
+}
+
